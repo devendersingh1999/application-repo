6fa205d9aac3daa1261240f8cde1a7f43184102e
devendersingh1999 <devendersingh29121999@gmail.com>
2025-11-13 07:26:19 +0000

    webhook add

--- app-new.sh
+++ app-new.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "this are new changes and part of are new brancding and this is app-new branch"
-echo "today date is ${date}"
+echo "today date is $(date)"
 echo "hi how are you, welcome to jenkins"
 echo "added new line"
+echo "Testing webhook trigger $(date)"
